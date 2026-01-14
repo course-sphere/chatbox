ee7a239f9a2ff81fa66e10ec86c1d5af7ed4a302
namdang-fdp <namdangcoder@gmail.com>
2026-01-14 18:21:02 +0700

    feat: add dto for tracking user who send message

---
+++ ChatRoom.Client/Models/ChatMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ChatRoom.Client.Models
+{
+    public class ChatMessage
+    {
+        public string? Sender { get; set; }
+        public string? Content { get; set; }
+        public DateTime Time { get; set; }
+        public bool IsMe { get; set; }
+        public string? Color { get; set; }
+    }
+}
